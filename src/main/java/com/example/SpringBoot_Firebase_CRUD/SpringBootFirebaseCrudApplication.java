3c9a53e530eb8a8992d3459c1209ac665360dadb
LilaniRanjan <ranjanlilani@gmail.com>
2025-03-29 22:02:03 +0530

    first commit

---
+++ src/main/java/com/example/SpringBoot_Firebase_CRUD/SpringBootFirebaseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Firebase_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirebaseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirebaseCrudApplication.class, args);
+	}
+
+}
