57b6c4b739112f0d49bad0da535daad1965ac825
LilaniRanjan <ranjanlilani@gmail.com>
2025-03-29 22:15:43 +0530

    Firebase dependency added

--- src/main/java/com/example/SpringBoot_Firebase_CRUD/SpringBootFirebaseCrudApplication.java
+++ src/main/java/com/example/SpringBoot_Firebase_CRUD/SpringBootFirebaseCrudApplication.java
@@ -2,6 +2,8 @@ package com.example.SpringBoot_Firebase_CRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootFirebaseCrudApplication {
@@ -9,5 +11,4 @@ public class SpringBootFirebaseCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirebaseCrudApplication.class, args);
 	}
-
 }
